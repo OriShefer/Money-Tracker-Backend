a9865cd8980d469503a442198bba2d57fb410aca
OriShefer <ori150403@gmail.com>
2023-06-01 01:43:21 +0300

    specify local date string to be of israel time zone

--- Utils/date.js
+++ Utils/date.js
@@ -1,5 +1,5 @@
 
 exports.getCurrentDate = () => {
-    const array = new Date().toLocaleDateString().split('.')
+    const array = new Date().toLocaleDateString("he-il").split('.')
     return new Date(array[2], parseInt(array[1]) - 1, parseInt(array[0]) + 1);
 }
